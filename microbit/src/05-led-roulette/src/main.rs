2582fb86be3182fa5ed6cb839b86452c61c9af5c
Michael Kariya <michael.h.kariya@gmail.com>
2022-10-14 20:37:57 -0500

    TImer Test Roulette

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -3,16 +3,22 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use panic_halt as _;
+use rtt_target::{rtt_init_print, rprintln};
+use panic_rtt_target as _;
 use microbit::board::Board;
+use microbit::hal::timer::Timer;
 use microbit::hal::prelude::*;
 
 #[entry]
 fn main() -> ! {
+    rtt_init_print!();
+
     let mut board = Board::take().unwrap();
 
-    board.display_pins.col1.set_low().unwrap();
-    board.display_pins.row1.set_high().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+    loop {
 
-    loop {}
+        timer.delay_ms(1000u16);
+        rprintln!("1000 ms passed");
+    }
 }
