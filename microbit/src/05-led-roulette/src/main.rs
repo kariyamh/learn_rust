dba870d6600553b01b5d87114d9c58bf9dc6103e
Michael Kariya <michael.h.kariya@gmail.com>
2022-10-14 20:40:55 -0500

    Copy

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -12,12 +12,11 @@ use microbit::hal::prelude::*;
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
-
     let mut board = Board::take().unwrap();
 
     let mut timer = Timer::new(board.TIMER0);
-    loop {
 
+    loop {
         timer.delay_ms(1000u16);
         rprintln!("1000 ms passed");
     }
